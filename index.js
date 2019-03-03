ed484525a99e3a18f151ed8d246a4388eb8511aa
Oleh Martyn <olehmartyn@Olehs-MacBook-Pro.local>
2019-03-03 19:01:09 +0200

    task5-docker: replace swagger definitions with joi schema

--- index.js
+++ index.js
@@ -7,15 +7,42 @@ const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+var j2s = require('joi-to-swagger');
+const eventsSchema = require('./src/routes/events/schemes/event');
+const discoveriesSchema = require('./src/routes/discoveries/schemes/discovery');
+
 app.use(bodyParser.json());
 app.use('/', routes);
 app.use((err, req, res, next)=>{
     res.status(422).send({
         error: err.message
     })
-})
+});
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const withoutId = (schema) => {
+    const {id, ...restProps} = schema.properties;
+    return {...schema, properties: restProps};
+};
+
+const getDocument = () => {
+    const Event = j2s(eventsSchema).swagger;
+    const Discovery = j2s(discoveriesSchema).swagger;
+    const NewEvent = withoutId(Event);
+    const NewDiscovery = withoutId(Discovery);
+    const newDocument = {
+        ...swaggerDocument,
+        definitions: {
+            ...swaggerDocument.definitions,
+            Event,
+            NewEvent,
+            Discovery,
+            NewDiscovery
+        }
+    };
+    return newDocument;
+}
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(getDocument()));
 
 app.listen(port, host);
 
